28babc479794f73307bc95b01b1b5419728d8d09
sachin <exigent.sachin@hotmail.com>
2019-03-05 14:29:31 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p className="App-intro">
+          Hello World Sachin!
+          </p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
