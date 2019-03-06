899276e7500dbda4ac1bac1beb9c464aa4a00732
sachin <exigent.sachin@hotmail.com>
2019-03-06 12:56:25 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p className="App-intro">
-          Hello World Sachin!
+          Hello World... Changes on the Feature Branch!
           </p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
